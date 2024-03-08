004cf166283322845745cfbdc76722182ee5082e
Dani <perdan90@gmail.com>
2024-03-08 18:24:06 +0100

    feat(Tasques): create Tasques file and start adding functionality

---
+++ src/main/java/intercepting/administrador/Tasques.java
@@ -0,0 +1,12 @@
+package intercepting.administrador;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tasques {
+
+    private List<Filtre> tasques = new ArrayList<Filtre>();
+    public Tasques() {
+    }
+
+}
